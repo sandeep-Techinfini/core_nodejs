10e2bb697f7db07f084a3e926f1dc841a4bd877d
Mr.Tikendra <mrtikendra@Mr.Tikendra>
2022-05-27 19:07:59 +0530

    new one

--- index.js
+++ index.js
@@ -1,94 +1,92 @@
-const http =require("http")
-const https = require("https")
+const http = require("http");
+const https = require("https");
 const url = require("url");
-let config = require("./config")
-let fs = require("fs")
-const StringDecoder = require('string_decoder').StringDecoder;
+let config = require("./config");
+let lib = require("./lib/data");
+let fs = require("fs");
+const StringDecoder = require("string_decoder").StringDecoder;
 
+// lib.createDir("userData1", (info) => {
+//   console.log(info);
+// });
 
-
-
-const httpServer = http.createServer((req,res)=>{
-unifiedServer(req,res);
- 
-});     
-
-httpServer.listen(config.httpPort,()=>{
-    console.log("working on "+config.httpPort);
-
+// lib.create('userData', 'aaddmin',"sandeep",(error)=>{
+//     console.log("error is",error)
+// })
+lib.read("userData","aaddmin",(error,data)=>{
+  console.log(error,data)
 })
-let httpsServerOptions ={
-    "key": fs.readFileSync("./https/key.pem"),
-    "cert": fs.readFileSync("./https/cert.pem")
-}
-const httpsServer = https.createServer(httpsServerOptions,(req,res)=>{
-unifiedServer(req,res);
-
- 
-}); 
 
 
-httpsServer.listen(config.httpsPort,()=>(
-    console.log("working on "+config.httpsPort)
-    
-))
+const httpServer = http.createServer((req, res) => {
+  unifiedServer(req, res);
+});
 
+httpServer.listen(config.httpPort, () => {
+  console.log("working on " + config.httpPort);
+});
+let httpsServerOptions = {
+  key: fs.readFileSync("./https/key.pem"),
+  cert: fs.readFileSync("./https/cert.pem"),
+};
+const httpsServer = https.createServer(httpsServerOptions, (req, res) => {
+  unifiedServer(req, res);
+});
 
-let unifiedServer = (req,res)=>{
+httpsServer.listen(config.httpsPort, () =>
+  console.log("working on " + config.httpsPort)
+);
 
-    const parseurl = url.parse(req.url,true);
-    const header= req.headers
-    const method = req.method
-    const trimpath= parseurl.path.replace(/^\/+|\/+$/g,"")
-    const decoder = new StringDecoder("utf-8")
-    let buffer = "";
-     req.on("data",(data)=>{
-         buffer += decoder.write(data)
-         
-     })
-     req.on("end",()=>{
-         buffer += decoder.end();
-     console.log("Buffer:",buffer)
-    
+let unifiedServer = (req, res) => {
+  const parseurl = url.parse(req.url, true);
+  const header = req.headers;
+  const method = req.method;
+  const trimpath = parseurl.path.replace(/^\/+|\/+$/g, "");
+  const decoder = new StringDecoder("utf-8");
+  let buffer = "";
+  req.on("data", (data) => {
+    buffer += decoder.write(data);
+  });
+  req.on("end", () => {
+    buffer += decoder.end();
+    console.log("Buffer:", buffer);
+
     // url path
-    
-         let chosenHandler =typeof(router[trimpath]) !== "undefined" ?router[trimpath]:handler.notFound;
-         let data = {
-             "trimpath":trimpath,
-             "header":header,
-             "payload":buffer,
-             "method":method
-         }
-    
-    chosenHandler(data,(statusCode,payload)=>{
-        statusCode = typeof(statusCode)=="number"?statusCode:200;
-    
-        payload = typeof(payload) == "object" ? payload : {}
-        // ***********************************************************************************************
-    let payloadstring = JSON.stringify(payload);
-    res.setHeader("Content-Type","application/json");
-    res.writeHead(statusCode)  
-    res.end(payloadstring); 
-    console.log("URL: ",payloadstring.payload,trimpath);
-    
-    })
-    
-     })
 
-}
+    let chosenHandler =
+      typeof router[trimpath] !== "undefined"
+        ? router[trimpath]
+        : handler.notFound;
+    let data = {
+      trimpath: trimpath,
+      header: header,
+      payload: buffer,
+      method: method,
+    };
 
+    chosenHandler(data, (statusCode, payload) => {
+      statusCode = typeof statusCode == "number" ? statusCode : 200;
 
+      payload = typeof payload == "object" ? payload : {};
+      // ***********************************************************************************************
+      let payloadstring = JSON.stringify(payload);
+      res.setHeader("Content-Type", "application/json");
+      res.writeHead(statusCode);
+      res.end(payloadstring);
+      console.log("URL: ", payloadstring.payload, trimpath);
+    });
+  });
+};
 
+let handler = {};
 
-let handler ={}
+handler.sample = (data, callback) => {
+  callback(408, data);
+};
+handler.notFound = (data, callback) => {
+  callback(404);
+};
 
-handler.sample =(data,callback)=>{
-callback(408,data)
-}
-handler.notFound =(data,callback)=>{
-callback(404)
-}
- 
 let router = {
-    "sample":handler.sample
-}
+  sample: handler.sample,
+};
